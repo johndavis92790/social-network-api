1c47534d3a0a96dba5ae3410a87cb5ee8375fe2b
johndavis <johndavis92790@gmail.com>
2022-02-27 20:08:15 -0700

    setup initial server, created model files

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+// get total count of friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+
+const User = model("User", UserSchema);
+
+module.exports = User;
